94ccac39fa7a322baa478c8db9fa834980871c84
eng-asano <eng.y.asano@gmail.com>
2024-12-06 20:06:49 +0900

    feat: disconnect

---
+++ src/disconnect.ts
@@ -0,0 +1,87 @@
+import { DynamoDB, ApiGatewayManagementApi } from 'aws-sdk'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+
+// DynamoDBクライアントの初期化
+const dynamodb = new DynamoDB.DocumentClient()
+const sessionsTable = 'sessions'
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const connectionId = event.requestContext?.connectionId
+
+  if (!connectionId) {
+    return {
+      statusCode: 400,
+      body: 'Missing connectionId in the request context.',
+    }
+  }
+
+  const params = {
+    TableName: sessionsTable,
+    Key: {
+      connection_id: connectionId,
+    },
+  }
+
+  try {
+    const target = await dynamodb.get(params).promise()
+
+    // 接続情報を削除
+    await dynamodb.delete(params).promise()
+
+    // クエリ用のパラメータを設定
+    const scanParams = {
+      TableName: sessionsTable,
+      FilterExpression: '#attribute = :value',
+      ExpressionAttributeNames: {
+        '#attribute': 'team_code',
+      },
+      ExpressionAttributeValues: {
+        ':value': target.Item?.team_code,
+      },
+    }
+
+    // DynamoDBからセッション情報を取得
+    const { Items } = await dynamodb.scan(scanParams).promise()
+
+    if (!Items || Items.length === 0) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'No active connections found.' }),
+      }
+    }
+
+    // API Gateway Management APIのインスタンス作成
+    const apiGateway = new ApiGatewayManagementApi({
+      endpoint: `${event.requestContext.domainName}/${event.requestContext.stage}`,
+    })
+
+    // WebSocket 経由でメッセージを送信
+    const postPromises = Items.map(async (item) => {
+      try {
+        await apiGateway
+          .postToConnection({
+            ConnectionId: item.connection_id as string,
+            Data: JSON.stringify({ action: 'disconnect', targetUserId: target.Item?.user_id }),
+          })
+          .promise()
+      } catch (err) {
+        console.error(`Failed to send message to ${item.connection_id}:`, err)
+      }
+    })
+
+    // 全ての送信タスクを完了させる
+    await Promise.all(postPromises)
+
+    return {
+      statusCode: 200,
+      body: 'Disconnection and session removal successful!',
+    }
+  } catch (error) {
+    console.error('Error:', error)
+
+    return {
+      statusCode: 500,
+      body: 'Failed to process disconnection.',
+    }
+  }
+}
