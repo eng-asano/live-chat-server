4ce21a5de643c4c32a5e8f518847b1d0a63be05b
eng-asano <eng.y.asano@gmail.com>
2024-12-10 23:52:49 +0900

    refactor: change directory

--- src/connect.ts
+++ src/connect.ts
@@ -1,11 +1,10 @@
-import { DynamoDB, ApiGatewayManagementApi } from 'aws-sdk'
+import { DynamoDB, ApiGatewayManagementApi, AWSError } from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 // DynamoDB クライアントの初期化
 const dynamodb = new DynamoDB.DocumentClient()
 const sessionsTable = 'sessions'
 
-// Lambda ハンドラー
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const connectionId = event.requestContext?.connectionId
   const teamCode = event.queryStringParameters?.team_code
@@ -35,47 +34,52 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
 
     const scanParams = {
       TableName: sessionsTable,
-      FilterExpression: '#attribute = :value',
+      FilterExpression: '#team_code = :team_code',
       ExpressionAttributeNames: {
-        '#attribute': 'team_code',
+        '#team_code': 'team_code',
       },
       ExpressionAttributeValues: {
-        ':value': teamCode,
+        ':team_code': teamCode,
       },
     }
 
     // DynamoDBからセッション情報を取得
-    const { Items } = await dynamodb.scan(scanParams).promise()
+    const { Items = [] } = await dynamodb.scan(scanParams).promise()
 
-    if (!Items || Items.length === 0) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ error: 'No active connections found.' }),
-      }
-    }
+    // 全接続ユーザーの情報を作成
+    const activeUserIds = Items.map((item) => item.user_id)
 
     // API Gateway Management APIのインスタンス作成
     const apiGateway = new ApiGatewayManagementApi({
       endpoint: `${event.requestContext.domainName}/${event.requestContext.stage}`,
     })
 
-    // 全接続ユーザーの情報を作成
-    const activeUserIds = Items.map((item) => item.user_id)
-
-    // WebSocket 経由でメッセージを送信
+    // WebSocket経由でメッセージを送信
     const postPromises = Items.flatMap(async (item) => {
       // 確実に接続の完了が保証されていないので、自身のconnection_idは除外
-      if (item.connection_id === connectionId) return Promise.resolve([])
+      if (item.connection_id === connectionId) {
+        await Promise.resolve([])
+        return
+      }
 
       try {
-        return apiGateway
+        await apiGateway
           .postToConnection({
             ConnectionId: item.connection_id as string,
-            Data: JSON.stringify({ action: 'connect', activeUserIds }),
+            Data: JSON.stringify({ action: 'connect', data: { activeUserIds } }),
           })
           .promise()
       } catch (err) {
-        console.error(`Failed to send message to ${item.connection_id}:`, err)
+        // 切断された接続を削除
+        const awsError = err as AWSError
+        if (awsError.statusCode === 410) {
+          await dynamodb
+            .delete({
+              TableName: sessionsTable,
+              Key: { connection_id: item.connection_id },
+            })
+            .promise()
+        }
       }
     })
 
--- src/disconnect.ts
+++ src/disconnect.ts
@@ -1,4 +1,4 @@
-import { DynamoDB, ApiGatewayManagementApi } from 'aws-sdk'
+import { DynamoDB, ApiGatewayManagementApi, AWSError } from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 // DynamoDBクライアントの初期化
@@ -7,13 +7,6 @@ const sessionsTable = 'sessions'
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const connectionId = event.requestContext?.connectionId
-
-  if (!connectionId) {
-    return {
-      statusCode: 400,
-      body: 'Missing connectionId in the request context.',
-    }
-  }
 
   const params = {
     TableName: sessionsTable,
@@ -28,50 +21,54 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
     // 接続情報を削除
     await dynamodb.delete(params).promise()
 
-    // クエリ用のパラメータを設定
     const scanParams = {
       TableName: sessionsTable,
-      FilterExpression: '#attribute = :value',
+      FilterExpression: '#team_code = :team_code',
       ExpressionAttributeNames: {
-        '#attribute': 'team_code',
+        '#team_code': 'team_code',
       },
       ExpressionAttributeValues: {
-        ':value': target.Item?.team_code,
+        ':team_code': target.Item?.team_code,
       },
     }
 
     // DynamoDBからセッション情報を取得
-    const { Items } = await dynamodb.scan(scanParams).promise()
+    const { Items = [] } = await dynamodb.scan(scanParams).promise()
 
-    if (!Items || Items.length === 0) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'No active connections found.' }),
-      }
-    }
+    // 全接続ユーザーの情報を作成
+    const activeUserIds = Items.map((item) => item.user_id)
 
     // API Gateway Management APIのインスタンス作成
     const apiGateway = new ApiGatewayManagementApi({
       endpoint: `${event.requestContext.domainName}/${event.requestContext.stage}`,
     })
 
-    // 全接続ユーザーの情報を作成
-    const activeUserIds = Items.map((item) => item.user_id)
-
-    // WebSocket 経由でメッセージを送信
+    // WebSocket経由でメッセージを送信
     const postPromises = Items.map(async (item) => {
-      // 確実に接続が残っていると保証されていないので、自身のconnection_idは除外
-      // if (item.connection_id === connectionId) return Promise.resolve([])
+      // 既に接続が切断されているので、自身のconnection_idは除外
+      if (item.connection_id === connectionId) {
+        await Promise.resolve([])
+        return
+      }
 
       try {
-        return apiGateway
+        await apiGateway
           .postToConnection({
             ConnectionId: item.connection_id as string,
-            Data: JSON.stringify({ action: 'disconnect', activeUserIds }),
+            Data: JSON.stringify({ action: 'disconnect', data: { activeUserIds } }),
           })
           .promise()
       } catch (err) {
-        console.error(`Failed to send message to ${item.connection_id}:`, err)
+        // 切断された接続を削除
+        const awsError = err as AWSError
+        if (awsError.statusCode === 410) {
+          await dynamodb
+            .delete({
+              TableName: sessionsTable,
+              Key: { connection_id: item.connection_id },
+            })
+            .promise()
+        }
       }
     })
 
